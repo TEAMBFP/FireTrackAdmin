8863cc7ae05ce75cb7048f383ef081d6aecd803e
abegailabragantrani <dolphinaeium@gmail.com>
2024-01-14 15:03:16 +0800

    change password validation of current password

--- src/pages/ChangePass.jsx
+++ src/pages/ChangePass.jsx
@@ -5,6 +5,8 @@ const ChangePass = () => {
     const user = JSON.parse(localStorage.getItem('user'))
     const [password, setPassword] = React.useState('');
     const [confirm_password, setConfirmPassword] = React.useState('');
+    const [current_password, setCurrentPassword] = React.useState('');
+    const [error, setError] = React.useState(null);
 
    const handleChangePassword = async () => { 
         if(password !== confirm_password){
@@ -14,16 +16,35 @@ const ChangePass = () => {
         }
         const payload = { 
             password, 
+            current_password,
             id:user.id, 
         };
 
-        await apiService.post('/change-pass', payload );
+        try {
+             await apiService.post('/change-pass', payload );
+             alert('Password changed successfully');
+             setError(null);
+        } catch (error) {
+            if (error.response.data.msg) {
+                setError(error.response.data.msg);
+            }
+            console.log(error.response);
+            
+        }
+       
         setPassword('');
         setConfirmPassword('');
-        return alert('Password changed successfully');
+        setCurrentPassword('');
+        
     }
   return (
     <div style={{display:'flex', flexDirection:'column', rowGap:'13px', padding:'42px'}}>
+        Current password
+        <input
+            value={current_password}
+            type='password'
+            onChange={(e)=>setCurrentPassword(e.target.value)}
+        />
         New password
         <input
             value={password}
@@ -36,6 +57,7 @@ const ChangePass = () => {
             type='password'
             onChange={(e)=>setConfirmPassword(e.target.value)}
         />
+        {error && <p style={{color:'red'}}>{error}</p>}
         <button
             style={{marginTop:'22px', width:'50%'}}
             onClick={handleChangePassword}
