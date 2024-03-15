51d64e935193e3906758b514dafe17a3f4b5468f
Ashly John Galagnara <80528305+LyAsh123@users.noreply.github.com>
2024-03-15 13:53:49 +0800

    wew

--- src/api/index.js
+++ src/api/index.js
@@ -4,11 +4,7 @@ import Axios from 'axios';
 // CHANGE THE IP ADDRESS BELOW TO YOUR LOCAL IP ADDRESS
 // In my case my local IP address is 192.168.1.31
 
-<<<<<<< HEAD
 const baseURL = 'http://192.168.56.1:8000/api'
-=======
-const baseURL = 'http://localhost:8000/api'
->>>>>>> c9608f6729ae139d4023d1a9df92bfa4bc7a8b04
 const apiService =  Axios.create({
   baseURL: baseURL,
 });
